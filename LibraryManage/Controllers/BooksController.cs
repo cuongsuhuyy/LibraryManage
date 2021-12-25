81f8811d588d92973d1beed9de358c3e3707c6ef
CuongSu <73159675+cuongsuhuyy@users.noreply.github.com>
2021-12-26 02:17:33 +0700

    Update data books

--- LibraryManage/Controllers/BooksController.cs
+++ LibraryManage/Controllers/BooksController.cs
@@ -25,5 +25,27 @@ namespace LibraryManage.Controllers
 
             return View("BookEdit", bookInfor);
         }
+
+        public ActionResult Update(string IdBook, string Name, string Description, int PublishingYear, string PublishingLocation, 
+            string Type, DateTime DateAddToLibrary, string Location, string Notes, string PathImage)
+        {
+            LibraryDBEntities db = new LibraryDBEntities();
+            var bookUpdate = db.Books.Single(x => x.ID_Book == IdBook);
+
+            bookUpdate.Name = Name;
+            bookUpdate.Description = Description;
+            bookUpdate.Publishing_Year = PublishingYear;
+            bookUpdate.Publishing_Location = PublishingLocation;
+            bookUpdate.Type = Type;
+            bookUpdate.Date_add_to_library = DateAddToLibrary;
+            bookUpdate.Location_in_library = Location;
+            bookUpdate.Notes = Notes;
+            bookUpdate.PathImage = PathImage;
+
+            db.Entry(bookUpdate);
+            db.SaveChanges();
+
+            return Content("Update successful");
+        }
     }
 }
