a34beb6a0aef9144fc85a3d85b5482fba4a7c303
CuongSu <73159675+cuongsuhuyy@users.noreply.github.com>
2021-12-29 03:15:08 +0700

    add create book and need to fix edit and delete, too

--- LibraryManage/Controllers/BooksController.cs
+++ LibraryManage/Controllers/BooksController.cs
@@ -57,17 +57,50 @@ namespace LibraryManage.Controllers
             return View("BookDetail", bookInfor);
         }
 
-        public ActionResult Delete(string IdBook, string Flag)
+        public ActionResult Delete(string IdBook)
         {
             LibraryDBEntities db = new LibraryDBEntities();
-            if (Flag == "true")
-            {
-                var bookInfor = db.Books.Single(x => x.ID_Book == IdBook);
+            var bookInfor = db.Books.Single(x => x.ID_Book == IdBook);
 
-                db.Books.Remove(bookInfor);
-                db.SaveChanges();
+            db.Books.Remove(bookInfor);
+            db.SaveChanges();
+
+            return RedirectToAction("Index", "Books");
+        }
+                
+        public ActionResult Create()
+        {            
+            return View("BookCreate");
+        }
+
+        [HttpPost]
+        public ActionResult AddNewToDB(string IdBook, string Name, string Description, int PublishingYear, string PublishingLocation,
+            string Type, string DateAddToLibrary, string Location, string Notes, string PathImage, int Quantily)
+        {
+            LibraryDBEntities db = new LibraryDBEntities();
+
+            var checkIfExist = db.Books.SingleOrDefault(x => x.ID_Book == IdBook);
+            if(checkIfExist != null)
+            {
+                return Content("This ID Book have been created");
             }
 
+            Book bookInfo = new Book();
+
+            bookInfo.ID_Book = IdBook;
+            bookInfo.Name = Name;
+            bookInfo.Description = Description;
+            bookInfo.Publishing_Year = PublishingYear;
+            bookInfo.Publishing_Location = PublishingLocation;
+            bookInfo.Type = Type;
+            bookInfo.Date_add_to_library = DateAddToLibrary;
+            bookInfo.Location_in_library = Location;
+            bookInfo.Notes = Notes;
+            bookInfo.PathImage = PathImage;
+            bookInfo.Quantily = Quantily;
+
+            db.Books.Add(bookInfo);
+            db.SaveChanges();
 
             return RedirectToAction("Index", "Books");
         }
